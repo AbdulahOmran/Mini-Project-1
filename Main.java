2c43f0a02bc85c432a87ca57cee7bbe51da10010
Abdulah Omran <abdulah.omran@patelhs.org>
2020-09-22 15:59:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+  int myAge = 13;
+  int myBirthday = 822;
+  int todaysDate = 921;
+  if (todaysDate > myBirthday)
+  {
+System.out.println("I am " + myAge + " years old");
+  }
+  else
+  {
+System.out.println("I am still " + myAge + " years old");
+  }
+  }
+}
