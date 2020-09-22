4f775e4dab21561a59f195a850a3c75d121e1c45
Abdulah Omran <abdulah.omran@patelhs.org>
2020-09-22 16:12:16 +0000

    updated if statement

--- Main.java
+++ Main.java
@@ -5,8 +5,9 @@
   int myAge = 13;
   int myBirthday = 822;
   int todaysDate = 921;
-  if (todaysDate > myBirthday)
+  if (todaysDate == myBirthday)
   {
+    myAge++;
     System.out.println("I am " + myAge + " years old");
   }
   else
